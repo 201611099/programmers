e8f396974bac07ac5acf9c80369d78957d33baee
Hyojeong Lee <hyojlee@c8r8s1.42seoul.kr>
2022-11-28 16:23:40 +0900

    Add string substr, algorithm sort, string to_string

---
+++ level1/12919_서울에서김서방찾기.cpp
@@ -0,0 +1,28 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12919
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+string solution(vector<string> seoul)
+{
+	sort(seoul.begin(), seoul.end());
+	vector<string>::iterator it = find(seoul.begin(), seoul.end(), "Kim");
+	return "김서방은 " + to_string(it - seoul.begin()) + "에 있다";
+}
+
+// string solution(vector<string> seoul) {
+// 	vector<string>::iterator it;
+
+// 	it = find(seoul.begin(), seoul.end(), "Kim");
+// 	string answer = "김서방은 " + to_string(it - seoul.begin()) + "에 있다";
+// 	return answer;
+// }
+
+int main(void)
+{
+	cout << solution({"Jane", "Kim"}) << endl;
+}
---
+++ level1/12948_핸드폰번호가리기.cpp
@@ -0,0 +1,35 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12948
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+string solution(string phone_number) {
+	string ret = "";
+
+	for (int i = 0; i < phone_number.size() - 4; i++)
+		ret += "*";
+	ret += phone_number.substr(phone_number.size() - 4, 4);
+	return (ret);
+}
+
+// string solution(string phone_number) {
+// 	if (phone_number.length() <= 4)
+// 		return phone_number;
+// 	string answer = "";
+// 	int i = 0;
+// 	for (; i < phone_number.length() - 4; i++)
+// 		answer += "*";
+// 	while (phone_number[i])
+// 		answer += phone_number[i++];
+// 	return answer;
+// }
+
+int main(void)
+{
+	cout << solution("1234") << endl; // 1234
+	cout << solution("01033334444") << endl; // *******4444
+	cout << solution("027778888") << endl; // *****8888
+	return 0;
+}
